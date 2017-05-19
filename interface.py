fcfed5cf2f05ae6bfc9813084443f330b0d88a39
bfaure <bfaure23@gmail.com>
2017-05-19 16:09:00 -0400

    interface now works

--- interface.py
+++ interface.py
@@ -12,187 +12,84 @@ import numpy as np
 from math import ceil,floor
 from scipy import ndimage
 
-#from keras.models import load_model
-#from keras import backend as K
-
-#from main import image_t
 import main
 from main import *
 
-SEARCH_DISTANCE = 6
+class predict_worker(object):
+	def __init__(self,parent=None,model_f="model/conv2D_classifier.h5"):
+		self.cur_model = load_model(model_f)
+		self.parent=parent
 
-def get_hit_proximity(x, y, x_vals, y_vals):
-
-	x_search_range = []
-	y_search_range = []
-
-	for i in range(SEARCH_DISTANCE*2):
-		x_search_range.append(SEARCH_DISTANCE+x-i)
-		y_search_range.append(SEARCH_DISTANCE+y-i)
-
-	x_index = -SEARCH_DISTANCE
-	best = 1000
-	for x_targ in x_search_range:
-		y_index = -SEARCH_DISTANCE
-		for y_targ in y_search_range:
-			for x_val,y_val in list(zip(x_vals, y_vals)):
-				if x_val==x_targ and y_val==y_targ:
-					proximity = abs(x_index*y_index)
-					if proximity < best:
-						best = proximity
-			y_index += 1
-		x_index += 1
-
-	return best
-
-
-class execution_thread(QThread):
-	# Thread to handle the calculations and inferface with the Keras classification modules.
-	def __init__(self):
-		QThread.__init__(self)
-
-	def run(self):
-		self.cur_model = load_model("model/conv2D_classifier.h5")
-
-	def process_data(self):
-		self.emit(SIGNAL("send_update(QString)"), "Constructing Image...")
+	def process_data(self,data):
+		self.cur_data = data
+		self.parent.results.setText("Calculating...")
 		
 		self.x_pos = self.cur_data.x_pos
 		self.y_pos = self.cur_data.y_pos
 
-		#proximity_levels = [150.0, 130.0, 90.0, 45.0, 30.0, 25, 20, 15, 10, 5, 1]
-		#proximity_depths = [25.0, 30.0, 40.0, 50.0, 80.0, 100.0, 150.0, 175.0, 200.0, 225.0, 255.0]
+		buf = 4 # 4 buffer on all sides
 
-		proximity_depths = [80.0, 100.0, 120.0, 160.0, 180.0, 200.0, 220.0, 255.0]
-		proximity_levels = [8, 7, 6, 5, 4, 3, 2, 1]
+		max_x = max(self.x_pos)
+		min_x = min(self.x_pos)
+		max_y = max(self.y_pos)
+		min_y = min(self.y_pos)
 
-		smallest_x 	= 1000
-		largest_x 	= 0
-		smallest_y 	= 1000
-		largest_y 	= 0
+		x_span = float(max_x-min_x)
+		y_span = float(max_y-min_y) 
 
-		for x,y in list(zip(self.x_pos, self.y_pos)):
-			if x > largest_x:
-				largest_x = x
-			if x < smallest_x:
-				smallest_x = x
-			if y > largest_y:
-				largest_y = y
-			if y < smallest_y:
-				smallest_y = y
+		for i in range(len(self.x_pos)):
+			self.x_pos[i] = (self.x_pos[i]-min_x)/x_span*20.0+buf
+			self.y_pos[i] = (self.y_pos[i]-min_y)/y_span*20.0+buf
 
-		translated_x = []
-		translated_y = []
+		pixels = []
 
-		for x,y in list(zip(self.x_pos, self.y_pos)):
-			translated_x.append(x-smallest_x)
-			translated_y.append(y-smallest_y)
-
-		x_size = largest_x - smallest_x
-		y_size = largest_y - smallest_y
-
-		x_scale = int(ceil(float(x_size/20)))
-		y_scale = int(ceil(float(y_size/20)))
-
-		#new_image = data.image()
-		pixels=[]
-
-		pixels2D = [[0.0]*28]*28
-
-		for x in range(28):
-			x_upscaled = (x-4)*x_scale
-			
-			if x < 5 or x > 25:
-				x_skip = True
-			else:
-				x_skip = False
-			
-			for y in range(28):
-				
-				if x_skip:
+		for y in range(28):
+			for x in range(28):
+				f_len = len(pixels)
+				for p_x,p_y in zip(self.x_pos,self.y_pos):
+					if int(p_x) in [x-1,x,x+1] and int(p_y) in [y-1,y,y+1]:
+						pixels.append(1.0)
+						break
+				if f_len==len(pixels):
 					pixels.append(0.0)
-					#new_image.add_pixel_XY(0.0, x, y)
-					#pixels2D[x][y]=0.0
-					continue
 
-				if y < 5 or y > 25:
-					pixels.append(0.0)
-					#new_image.add_pixel_XY(0.0, x, y)
-					#pixels2D[x][y]=0.0
-					continue
-				
-				y_upscaled = (y-4)*y_scale
-
-				level = get_hit_proximity(x_upscaled, y_upscaled, translated_x, translated_y)
-				value = 0.0
-
-				for levels, depths in list(zip(proximity_levels, proximity_depths)):
-
-					if level <= levels:
-						value = depths
-
-				#new_image.add_pixel_XY(value, x, y)
-				pixels.append(value)
-				#pixels2D[x][y]=value
-
-		#for y in range(28):
-		#	for x in range(28):
-		#		pixels.append(pixels2D[y][x])
-
-		new_image = image_t(pixels)
-		
-		#for i in range(len(new_image.pixels)):
-		#	new_image[i] = list(reversed(new_image.pixels[i]))
-
-		new_image.output_terminal()
-		new_image.label = 0
+		image = image_t(pixels)
+		image.output_terminal(threshold=0.9)
 
 		if K.image_data_format()=='channels_first':
 			input_shape = (1,28,28)
 		else:
 			input_shape = (28,28,1)
 
-		new_image.normalize()
-
-		image = np.array([np.array(new_image.pixels).reshape(input_shape)])
-		#proba = self.cur_model.predict_proba(image)
-		prediction = self.cur_model.predict(image,batch_size=1,verbose=1)
-		print(prediction)
-		self.emit(SIGNAL("send_update(QString)"),"Digit is NULL")
-		return
-
-		highest_prob = 0.0
-		cur_index = 0
-		highest_prob_index = 0
-		for probability in proba[0]:
-			if probability > highest_prob:
-				highest_prob = probability
-				highest_prob_index = cur_index
-			cur_index += 1
-
-		self.emit(SIGNAL("send_update(QString)"), "Digit is a "+str(highest_prob_index)+" with probability of "+str(highest_prob))
-		return
+		image = np.array([np.array(image.pixels).reshape(input_shape)])
+		prediction = self.cur_model.predict(image,batch_size=1,verbose=0)
 		
-		
-	def get_data(self, path):
-		# Need to figure out the bounds of the image (the maximums in all directions)
-		self.cur_data = path
-		self.process_data()
+		highest_prob = 0.0 
+		highest_prob_val = None
+
+		i=0
+		for p in prediction[0]:
+			if p>highest_prob:
+				highest_prob=p 
+				highest_prob_val=i
+			i+=1
+
+		self.parent.results.setText("Digit is "+str(highest_prob_val)+", prob = %0.2f"%(100.0*highest_prob))
 
 class drawing_path():
 	def __init__(self):
 		self.x_pos = []
 		self.y_pos = []
+
 	def add_point(self, x, y):
-		# Adds a single point to the path
 		self.x_pos.append(x)
 		self.y_pos.append(y)
+
 	def clear_path(self):
-		# Clears both the x and y components of the path
 		self.x_pos = []
 		self.y_pos = []
+
 	def print_path(self):
-		# Outputs a represenation of the path to the terminal
 		smallest_x 	= 1000
 		largest_x 	= 0
 		smallest_y 	= 1000
@@ -233,24 +130,12 @@ class drawing_path():
 
 
 class window(QtGui.QWidget):
-	# Window to allow user to input hand written digits for the system to analyze.
-	# Basic idea is I am going to create a widget to allow the user to write in a digit
-	# and when the user is done the system will gather the user input and send a signal to
-	# a slot in the execution_thread which will allow it to run the Keras model and send back
-	# a prediction of the digit classification.
 	def __init__(self, parent=None):
 		super(window, self).__init__()
+		self.worker = predict_worker(parent=self)
+		self.init_ui()
 
-		self.initThread()
-
-	def initThread(self):
-		# Initializes the thread and starts its execution (loading in the model)
-		self.thread = execution_thread()
-		self.thread.start()
-		self.initUI()
-
-	def initUI(self):
-		# Initializes the GUI
+	def init_ui(self):
 		self.setFixedHeight(600)
 		self.setFixedWidth(450)
 		self.setWindowTitle("Dynamic Digit Prediction")
@@ -263,11 +148,6 @@ class window(QtGui.QWidget):
 
 		self.rect = QRect(0, 50, 400, 400)
 
-		#self.drawing = QtGui.QPainter(self) # Device to allow user input
-		#self.drawing.mousePressEvent.connect(self.start_drawing) # User presses mouse button
-		#self.drawing.mouseMoveEvent.connect(self.drawing_occured) # User moving the mouse
-		#self.drawing.mouseReleaseEvent.connect(self.end_drawing) # User lets go of mouse button
-
 		self.label = QtGui.QLabel("Click and hold the left mouse button to draw a digit (0-9)", self)
 		self.label.move(5, 10)
 		self.label.setFixedWidth(300)
@@ -276,7 +156,6 @@ class window(QtGui.QWidget):
 		self.results.setFixedWidth(300)
 		self.result_label = QtGui.QLabel("", self)
 		self.result_label.move(330, 490)
-
 
 		self.clear_button = QtGui.QPushButton("Clear", self)
 		self.clear_button.move(330, 535)
@@ -301,10 +180,6 @@ class window(QtGui.QWidget):
 		self.right_line.setFrameShape(QFrame.VLine)
 		self.right_line.move(375, 100)
 		self.right_line.setFixedHeight(400)
-
-
-		QtCore.QObject.connect(self, QtCore.SIGNAL("send_data(PyQt_PyObject)"), self.thread.get_data)
-		QtCore.QObject.connect(self.thread, QtCore.SIGNAL("send_update(QString)"), self.update_label)
 
 		self.show()
 
@@ -343,14 +218,11 @@ class window(QtGui.QWidget):
 		if 100 < y < 500:
 			if 25 < x < 425:
 				if self.mouseHeld == True:
-
 					position = event.pos()
 					self.path.add_point(x,y)
 					self.results.setText("Position = "+str(position))
 					self.update()
-					return
-				else:
-					return
+				return
 			else:
 				self.results.setText("Position out of range")
 		else:
@@ -376,8 +248,7 @@ class window(QtGui.QWidget):
 	def mouseReleaseEvent(self, event):
 		self.mouseHeld = False
 		self.results.setText("Processing Data...")
-		self.emit(SIGNAL("send_data(PyQt_PyObject)"), self.path)
-		#self.path.clear_path()
+		self.worker.process_data(self.path)
 
 	def update_label(self, text):
 		self.results.setText(text)
